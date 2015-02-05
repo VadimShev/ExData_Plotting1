e43ea041ac618ced5c2cec2abd4c5e6752adf18e
Vadim <VadimShev@users.noreply.github.com>
2015-02-06 01:02:45 +0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -7,14 +7,14 @@ tabvar$Date <- strptime(paste(tabvar$Date,tabvar$Time), "%d/%m/%Y %H:%M:%S")
 tab<-tabvar[(tabvar$Date>="2007-02-01 00:00:00" & tabvar$Date<"2007-02-03 00:00:00"),]
 
 par(mfrow = c(1, 1), pch=".", bg = "transparent")
-
+png("plot3.png", height = 480, width = 480, bg = "transparent")
 with(tab, {
-        plot(Date,  Sub_metering_1, xlab="",  ylab=" ", ylim=c(0,38))
+        plot(Date,  Sub_metering_1, pch=".", xlab="",  ylab=" ", ylim=c(0,38))
         lines(Date, Sub_metering_1, lwd=1, ylim=c(0,38) )
 })       
 par(new=T)
 with(tab, {
-         plot(Date,  Sub_metering_2, xlab="", ylab=" ", col="red", ylim=c(0,38))
+         plot(Date,  Sub_metering_2, pch=".", xlab="", ylab=" ", col="red", ylim=c(0,38))
          lines(Date, Sub_metering_2, lwd=1, col="red", ylim=c(0,38))
 })   
 par(new=T)
@@ -24,6 +24,9 @@ with(tab, {
 })
 legend("topright", lwd=1, col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 ## Create plot on screen device 
+
+dev.off() ##  close the PNG device!
+
 
 dev.copy(png, file = "plot3.png",  width=480, height=480) ## Copy my plot to a PNG file
 dev.off() ##  close the PNG device!
