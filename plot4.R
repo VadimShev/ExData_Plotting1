d1ef3a95d85c4e698dd13f54c8a9f2ec6e4b6439
Vadim <VadimShev@users.noreply.github.com>
2015-02-06 01:03:50 +0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -6,6 +6,7 @@ tabvar$Date <- strptime(paste(tabvar$Date,tabvar$Time), "%d/%m/%Y %H:%M:%S")
 
 tab<-tabvar[(tabvar$Date>="2007-02-01 00:00:00" & tabvar$Date<"2007-02-03 00:00:00"),]
 
+png("plot4.png", height = 480, width = 480, bg = "transparent")
 par(mfrow = c(2, 2), pch=".", bg = "transparent", cex=0.5)
 
 with(tab, {
@@ -40,5 +41,4 @@ with(tab, {
   lines(Date, Global_reactive_power, lwd=1)
 })
 
-dev.copy(png, file = "plot4.png",  width=480, height=480) ## Copy my plot to a PNG file
 dev.off() ##  close the PNG device!
